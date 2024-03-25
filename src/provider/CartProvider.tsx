b6f437c787268603dd2a336c1fa5581572c0ef34
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2024-03-25 15:34:30 +0300

    Total value of items in cart is calculated and button to checkout is added

--- src/provider/CartProvider.tsx
+++ src/provider/CartProvider.tsx
@@ -7,6 +7,7 @@ export type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 //Creating a context that will hold the variables
@@ -14,6 +15,7 @@ const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -56,9 +58,14 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     );
   };
 
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
+
   //Wrapper for our provider
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
