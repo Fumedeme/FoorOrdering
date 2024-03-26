5488479c521f8e345ece854e797e5b158cacead1
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2024-03-26 16:02:54 +0300

    Added topBarNavigator for admin side inside Orders screen

---
+++ src/app/(admin)/orders/list/archive.tsx
@@ -0,0 +1,21 @@
+import { FlatList, StyleSheet } from "react-native";
+import { View } from "@/components/Themed";
+import orders from "@assets/data/orders";
+import OrderListItem from "@/components/OrderListItem";
+
+export default function ArchiveScreen() {
+  return (
+    <FlatList
+      data={orders}
+      renderItem={({ item }) => <OrderListItem orderItem={item} />}
+      contentContainerStyle={styles.container}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+  },
+});
